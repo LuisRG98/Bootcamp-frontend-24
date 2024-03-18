39b89a56eac0092b1d1a48241f802102883eb7cc
luis rivas <luisricardorivasgiwencer@gmail.com>
2024-03-18 13:58:28 -0400

    code refactor

--- JS-challenge-2/exercise5.js
+++ JS-challenge-2/exercise5.js
@@ -1,18 +1,13 @@
 function findSingleElement(arr) {
-  const uniqueElements = new Set();
-  const repeatedElements = new Set();
-
-  for (let num of arr) {
-    if (uniqueElements.has(num)) {
-      repeatedElements.add(num);
-      uniqueElements.delete(num);
-    } else if (!repeatedElements.has(num)) {
-      uniqueElements.add(num);
-    }
-  }
-
-  return Array.from(uniqueElements)[0];
+  const countMap = arr.reduce(
+    (map, num) => map.set(num, (map.get(num) || 0) + 1),
+    new Map()
+  );
+  const uniqueElement = Array.from(countMap.keys()).filter(
+    (num) => countMap.get(num) === 1
+  );
+  return uniqueElement[0];
 }
 
 const arr = [1, 2, 4, 7, 4, 2, 1];
-console.log(findSingleElement(arr)); // Output: 2
+console.log(findSingleElement(arr)); // Output: 7
