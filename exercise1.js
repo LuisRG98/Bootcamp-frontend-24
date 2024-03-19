dd5b1e827331e506fc9ec7cee6a32aef1bfd67a4
luis rivas <luisricardorivasgiwencer@gmail.com>
2024-03-19 17:31:48 -0400

    CODE IMPROVE

---
+++ exercise1.js
@@ -0,0 +1,35 @@
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new HttpError(response);
+  }
+  return response.json();
+}
+
+async function demoGithubUser() {
+  let name;
+  do {
+    name = prompt("Enter a name?", "vanimar");
+    try {
+      const user = await loadJson(`https://api.github.com/users/${name}`);
+      alert(`Full name: ${user.name}.`);
+      return user;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status === 404) {
+        alert("No such user, please reenter.");
+      } else {
+        throw err;
+      }
+    }
+  } while (true);
+}
+
+demoGithubUser();
